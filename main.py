3f310b84d85ec319598d224bd96d8cf7ab1b41b3
Vo Huy Nguyen <huynguyenvo2001@gmail.com>
2022-05-07 00:27:31 +0700

    conflict 2

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def cong(a, b):
+def tru(c, d):
     a = 2
     b = 4
     return 0
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    cong(1, 2)
+    tru(1, 2)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
